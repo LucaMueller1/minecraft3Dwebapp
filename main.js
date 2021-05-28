8cfd896590a807b4c462a80114292e74a80dea29
REIGNjs <unconfigured@null.spigotmc.org>
2021-05-29 01:46:43 +0200

    added light spawing

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import './style.css';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {THREEx} from './scripts/threex.daynight';
+import { Vector3 } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -21,12 +22,10 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
+/*
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
-ambientLight.intensity = 0;
-const pointLight = new THREE.PointLight(0xFFFFFF);
-pointLight.intensity = 100;
-pointLight.position.set(5,50,5);
-//scene.add(ambientLight, pointLight);
+ambientLight.intensity = 10;
+scene.add(ambientLight);*/
 
 //day/night cycle
 
@@ -50,34 +49,38 @@ scene.add(starField.object3d);
 //helpers
 const controls = new OrbitControls(camera, renderer.domElement);
 const gridHelper = new THREE.GridHelper(200, 50);
-const lightHelper = new THREE.PointLightHelper(pointLight);
 
-scene.add(gridHelper, lightHelper);
+scene.add(gridHelper);
 
 function randomIntFromInterval(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const spawnLights = (count) => {
-  for(let i = 0; i < count; i++) {
-    let light = new THREE.PointLight(0xFF9900);
-    const lightHelper = new THREE.PointLightHelper(light);
-    light.position.set(randomIntFromInterval(-50, 50), 30, randomIntFromInterval(-50, 50));
-    light.intensity = 50;
-    scene.add(light, lightHelper);
+let torches = [];
+const spawnLights = (object) => {
+  if(!object.name.includes("main") && !object.name.includes("Scene")) {
+    let torchNumber = object.name.replace( /^\D+/g, '');
+    if(torchNumber % 4 == 0) {
+      console.log(object);
+      let light = new THREE.PointLight(0xff5900);
+      light.position.set(object.position.x, object.position.y+1, object.position.z);
+      let helper = new THREE.PointLightHelper(light);
+      light.intensity = 30;
+      light.distance = 6;
+      light.decay = 1.4;
+      torches.push(light);
+      scene.add(light, helper);
+    } else {
+      object.visible = false;
+    }
+    
   }
 }
-
-spawnLights(10);
 
 //model import
 const loader = new GLTFLoader();
 
-var conditions = ["Water", "Redstone", "Torch", "torch", "emit", "crafting"];
-
 loader.load("./models/oneBlock.glb", function (gltf) {
-
-  console.log(gltf.scene.children[0].children);
 
   //gltf.scene.traverse(spawnLights);
 
@@ -94,7 +97,6 @@ loader.load("./models/oneBlock.glb", function (gltf) {
     }
   });
   */
-  console.log("------------ END ------------");
 	scene.add( gltf.scene );
 }, undefined, function (error) {
 	console.error(error);
@@ -102,20 +104,14 @@ loader.load("./models/oneBlock.glb", function (gltf) {
 
 
 loader.load("./models/watzz.glb", function (gltf) {
+	scene.add( gltf.scene );
+}, undefined, function (error) {
+	console.error(error);
+});
 
-  gltf.scene.traverse((o)=> {
-    console.log(o);
-    
-    if(o.isMesh && !(conditions.some(el => o.material.name.includes(el)))) {
-      if(o.material.name === "") {
-        return;
-      }
-      o.material.depthWrite = true;
-      console.log(o.material.name);
-    }
-  });
-  
-
+loader.load("./models/torches.glb", function (gltf) {
+  gltf.scene.visible = false;
+  gltf.scene.traverse(spawnLights);
 	scene.add( gltf.scene );
 }, undefined, function (error) {
 	console.error(error);
@@ -128,7 +124,7 @@ const animate = () => {
 
   controls.update();
 
-  sunAngle += 0.0002/dayDuration * Math.PI*2;
+  sunAngle += 0.002/dayDuration * Math.PI*2;
   starField.update(sunAngle);
   skydom.update(sunAngle);
   sunLight.update(sunAngle);
